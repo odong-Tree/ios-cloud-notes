bbeab0def6ee2392a9e707d7f9c9f1429b8f5b58
GREEN <humains29@gmail.com>
2021-02-15 20:19:54 +0900

    feat: Memo 모델 최종수정시간 Date타입 연산프로퍼티 생성

--- CloudNotes/CloudNotes/Model/Memo.swift
+++ CloudNotes/CloudNotes/Model/Memo.swift
@@ -4,5 +4,14 @@ struct Memo: Decodable {
     let title: String
     let body: String
     private let lastModified: Int
+    
+    var lastModifiedDate: Date {
+        return Date(timeIntervalSinceReferenceDate: TimeInterval(lastModified))
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case title, body
+        case lastModified = "last_modified"
+    }
 
 }
