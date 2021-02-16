f83ff452679e684871e2ac7788ba5cf5c1f1967e
GREEN <humains29@gmail.com>
2021-02-16 14:03:10 +0900

    feat: 뷰 컨트롤러에 2개의 리스트/컨텐츠 뷰 인스턴스 생성

--- CloudNotes/CloudNotes/ViewController/MemoViewController.swift
+++ CloudNotes/CloudNotes/ViewController/MemoViewController.swift
@@ -1,12 +1,12 @@
 import UIKit
 
 class MemoViewController: UIViewController {
+    
+    let memoListView = MemoListTableViewController()
+    let memoContentsView = MemoContentsViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-    
-
 }
 
