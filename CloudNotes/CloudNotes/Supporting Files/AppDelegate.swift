a641994303f727ad93b016388b1b03d2688230ff
GREEN <humains29@gmail.com>
2021-03-03 13:37:22 +0900

    feat: AppDelegate에서 SDK로 리디렉션 처리 메서드 추가

--- CloudNotes/CloudNotes/Supporting Files/AppDelegate.swift
+++ CloudNotes/CloudNotes/Supporting Files/AppDelegate.swift
@@ -78,6 +78,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let oauthCompletion: DropboxOAuthCompletion = {
+          if let authResult = $0 {
+              switch authResult {
+              case .success:
+                  print("Success! User is logged into DropboxClientsManager.")
+              case .cancel:
+                  print("Authorization flow was manually canceled by user!")
+              case .error(_, let description):
+                  print("Error: \(String(describing: description))")
+              }
+          }
+        }
+        let canHandleUrl = DropboxClientsManager.handleRedirectURL(url, completion: oauthCompletion)
+        return canHandleUrl
+    }
 
 }
 
